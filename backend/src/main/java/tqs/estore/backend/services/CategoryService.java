0e0b52a3e33730200439f287a68b31ba0eb59021
abutuc <andrebutuc@ua.pt>
2023-05-22 02:10:47 +0100

    DM-12 Added CategoryController boundary test

--- backend/src/main/java/tqs/estore/backend/services/CategoryService.java
+++ backend/src/main/java/tqs/estore/backend/services/CategoryService.java
@@ -1,6 +1,4 @@
 package tqs.estore.backend.services;
-
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import tqs.estore.backend.datamodel.PlantCategory;
 import tqs.estore.backend.repositories.CategoryRepository;
@@ -14,7 +12,7 @@ public class CategoryService {
     public CategoryService(CategoryRepository categoryRepository){
         this.categoryRepository = categoryRepository;
     }
-    public ResponseEntity<List<PlantCategory>> getAllCategories(){
+    public List<PlantCategory> getAllCategories(){
         return null;
     }
 }
