eb5b685e23fd82c00b4010b674b9293213a992de
abutuc <andrebutuc@ua.pt>
2023-05-30 14:28:27 +0100

    DM-16 added OrderService Unit Tests

--- backend/src/main/java/tqs/estore/backend/services/OrderService.java
+++ backend/src/main/java/tqs/estore/backend/services/OrderService.java
@@ -1,19 +1,31 @@
 package tqs.estore.backend.services;
 
 import org.springframework.stereotype.Service;
+import tqs.estore.backend.connection.DropMateAPIClient;
 import tqs.estore.backend.datamodel.Order;
 import tqs.estore.backend.datamodel.OrderDTO;
 import tqs.estore.backend.exceptions.InvalidOrderException;
 import tqs.estore.backend.repositories.OrderRepository;
+import tqs.estore.backend.repositories.PlantRepository;
+import tqs.estore.backend.repositories.UserRepository;
 
 import java.util.List;
 
 @Service
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
+    private final PlantRepository plantRepository;
+    private DropMateAPIClient dropMateAPIClient;
 
-    public OrderService(OrderRepository orderRepository) {
+
+    public OrderService(OrderRepository orderRepository, DropMateAPIClient dropMateAPIClient,
+                        UserRepository userRepository, PlantRepository plantRepository) {
         this.orderRepository = orderRepository;
+        this.dropMateAPIClient = dropMateAPIClient;
+        this.userRepository = userRepository;
+        this.plantRepository = plantRepository;
+
     }
 
     public Order createOrder(OrderDTO order) throws InvalidOrderException {
